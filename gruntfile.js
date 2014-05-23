02bfe40bcfd36d53043040e60875077d51507a65
DaZorni <sebi.zorn@googlemail.com>
2014-05-23 22:17:23 +0200

    Using grunt with less compiler.

---
+++ gruntfile.js
@@ -0,0 +1,52 @@
+/*!
+ * symfony-dazorni-standard Gruntfile
+ */
+module.exports = function(grunt){
+
+    'use strict';
+
+    // Force use of Unix newlines
+    grunt.util.linefeed = '\n';
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Project configuration.
+    grunt.initConfig({
+
+        // Metadata.
+        pkg: grunt.file.readJSON('package.json'),
+
+        less: {
+            compileCore: {
+                options: {
+                    strictMath: true,
+                    noColor: true,
+                    outputSourceFiles: true
+                },
+                files: {
+                    'web/public/css/<%= pkg.name %>.css': 'app/Resources/public/less/bootstrap.less'
+                }
+            },
+            minify: {
+                options: {
+                    cleancss: true,
+                    report: 'min'
+                },
+                files: {
+                    'web/public/css/<%= pkg.name %>.min.css': 'web/public/css/<%= pkg.name %>.css'
+                }
+            }
+        },
+
+    watch: {
+            less: {
+                files: '**/*.less',
+                tasks: ['less']
+            }
+        }
+    });
+
+    grunt.registerTask('buildless',  ['less']);
+    grunt.registerTask('default', ['buildless']);
+};
