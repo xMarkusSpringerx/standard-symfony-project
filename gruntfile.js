c89ad37b8915f0cdab5ae0c25a07ccc1013a880b
DaZorni <sebi.zorn@googlemail.com>
2014-05-27 21:24:55 +0200

    Nice indent for watch task in gruntfile.

--- gruntfile.js
+++ gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function(grunt){
             }
         },
 
-    watch: {
+        watch: {
             less: {
                 files: '**/*.less',
                 tasks: ['less']
